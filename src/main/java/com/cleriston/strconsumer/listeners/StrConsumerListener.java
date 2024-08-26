6f51f5851731540f084853ed6a7edf2c32d36538
Cleriston Lima <cleriston-santos2012@hotmail.com>
2024-08-26 13:54:13 -0300

    Treinamento finalizado de Kafka - str-consumer

--- src/main/java/com/cleriston/strconsumer/listeners/StrConsumerListener.java
+++ src/main/java/com/cleriston/strconsumer/listeners/StrConsumerListener.java
@@ -5,24 +5,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import lombok.extern.java.Log;
+import com.cleriston.strconsumer.custom.StrConsumerCustomListener;
+
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 	
-	@KafkaListener(groupId = "group-0", topics = "str-topic", containerFactory = "strContainerFactory")
+	@SneakyThrows
+	@StrConsumerCustomListener(groupId = "group-1")
 	public void create(String message) {
 		log.info("CREATE ::: Receive message {}", message);
+		throw new IllegalArgumentException("EXCEPTION...");
 	}
 	
-	@KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+	@StrConsumerCustomListener(groupId = "group-1")
 	public void log(String message) {
 		log.info("LOG ::: Receive message {}", message);
 	}
 
-	@KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+	@KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "validMessageContainerFactory")
 	public void history(String message) {
 		log.info("HISTORY ::: Receive message {}", message);
 	}
